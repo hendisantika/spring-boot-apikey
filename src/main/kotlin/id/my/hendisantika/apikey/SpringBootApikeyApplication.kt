6d0b7c5e39876e456705f5cdd62a3d3f2260d61b
hendisantika <hendisantika@yahoo.co.id>
2025-06-04 14:28:33 +0700

    feat: Initial Project🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/apikey/SpringBootApikeyApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.apikey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootApikeyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootApikeyApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/apikey/SpringBootApikeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.apikey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootApikeyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
