b6ef5cda509054bd113aebdad94b671363b87dd3
hendisantika <hendisantika@yahoo.co.id>
2025-06-04 14:36:41 +0700

    feat: Add createApiKey in ApiKeyController🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/apikey/controller/ApiKeyController.kt
+++ src/main/kotlin/id/my/hendisantika/apikey/controller/ApiKeyController.kt
@@ -1,6 +1,8 @@
 package id.my.hendisantika.apikey.controller
 
 import id.my.hendisantika.apikey.service.ApiKeyService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,4 +19,20 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/admin/api-keys")
-class ApiKeyController(private val apiKeyService: ApiKeyService)
+class ApiKeyController(private val apiKeyService: ApiKeyService) {
+    @PostMapping
+    fun createApiKey(@RequestBody request: CreateApiKeyRequest): ApiKeyResponse {
+        val apiKey = apiKeyService.createApiKey(
+            name = request.name,
+            role = request.role,
+            rateLimit = request.rateLimit
+        )
+        return ApiKeyResponse(
+            key = apiKey.key,
+            name = apiKey.name,
+            expiresAt = apiKey.expiresAt,
+            role = apiKey.role,
+            rateLimit = apiKey.rateLimit
+        )
+    }
+}
